138fc006571d06920f4c6abf8c4e309e27865a6b
mcabral <mariocabral@hotmail.es>
2020-11-24 23:29:12 -0300

    migrate react ui project to empty one

--- react-ui/app/auth/keycloak.js
+++
@@ -1,9 +0,0 @@
-import Keycloak from 'keycloak-js';
-import config from './../config'
-
-// Setup Keycloak instance as needed
-// Pass initialization options as required or leave blank to load from 'keycloak.json'
-const keycloak = Keycloak(config.keycloak);
-
-
-export default keycloak;
